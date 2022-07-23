b4fa204db7e26ba714e701bb5f417bef209b28fb
frodrickfronkenstein <ericandrewsmith88@gmail.com>
2022-07-23 12:09:48 -0400

    service worker added

---
+++ public/service-worker.js
@@ -0,0 +1,50 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+        './index.html',
+        './css/sytles.css',
+        './js/idb.js',
+        './js/index.js'
+];
+
+// return cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
+// cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+//delete outdated caches
+self.addEventListener('activate', function (keyList) {
+    let cacheKeepList = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+    })
+    cacheKeepList.push(CACHE_NAME);
+
+    return Promise.all(keyList.map(function (key, i) {
+        if (cacheKeepList.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i]);
+        }
+    }));
+})
